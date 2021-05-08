f5245bb45a0155a0f4dd5e85a00c2cff7e2af282
Jarrod Servilla <jarrod.servilla@gmail.com>
2021-05-08 15:30:03 -0400

    add validation

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,13 +1,13 @@
 import {
   Button,
   FormControl,
-  FormErrorMessage,
   FormHelperText,
   FormLabel,
   Input,
+  Text,
 } from "@chakra-ui/react";
 import { Form, withFormik } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import { defineMessages, useIntl } from "react-intl";
 import * as Yup from "yup";
 
@@ -42,16 +42,34 @@ const messages = defineMessages({
 });
 
 const RegisterSchema = Yup.object().shape({
-  email: Yup.string().email().required(),
+  email: Yup.string()
+    .email()
+    .matches(
+      /(^[A-Za-z0-9._%+-]+@mail.utoronto.ca$|^[A-Za-z0-9._%+-]+@utoronto.ca$)/,
+      "Email does not end with valid domain"
+    )
+    .required(),
   password: Yup.string().required(),
-  confirmPassword: Yup.string().required(),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required(),
 });
 
-const RegisterForm = ({ errors, isValid, setFieldValue }) => {
+const RegisterForm = ({
+  errors,
+  setFieldValue,
+  values: { email, password, confirmPassword },
+}) => {
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const isValid = email && password && confirmPassword;
   const { formatMessage } = useIntl();
   return (
-    <Form className="col-6">
-      <FormControl id="email" isRequired>
+    <Form className="col-6 w-25">
+      <FormControl
+        id="email"
+        isRequired
+        isInvalid={hasSubmitted && errors.email}
+      >
         <FormLabel>{formatMessage(messages.emailAddress)}</FormLabel>
         <Input
           type="email"
@@ -60,27 +78,40 @@ const RegisterForm = ({ errors, isValid, setFieldValue }) => {
         <FormHelperText>
           {formatMessage(messages.emailHelperText)}
         </FormHelperText>
-        <FormErrorMessage>{errors.email}</FormErrorMessage>
+        {hasSubmitted && <Text color="red">{errors.email}</Text>}
       </FormControl>
-      <FormControl id="password" isRequired mt={2}>
+      <FormControl
+        id="password"
+        isRequired
+        mt={2}
+        isInvalid={hasSubmitted && errors.confirmPassword}
+      >
         <FormLabel>{formatMessage(messages.password)}</FormLabel>
         <Input
           type="password"
           onChange={(e) => setFieldValue("password", e.target.value)}
         />
+        {hasSubmitted && <Text color="red">{errors.password}</Text>}
       </FormControl>
-      <FormControl id="confirmPassword" isRequired mt={2}>
+      <FormControl
+        id="confirmPassword"
+        isRequired
+        mt={2}
+        isInvalid={hasSubmitted && errors.confirmPassword}
+      >
         <FormLabel>{formatMessage(messages.confirmPassword)}</FormLabel>
         <Input
           type="password"
           onChange={(e) => setFieldValue("confirmPassword", e.target.value)}
         />
+        {hasSubmitted && <Text color="red">{errors.confirmPassword}</Text>}
       </FormControl>
       <Button
         className="w-100 mt-4"
         isDisabled={!isValid}
         colorScheme="green"
         type="submit"
+        onClick={() => setHasSubmitted(true)}
       >
         {formatMessage(messages.createAcct)}
       </Button>
@@ -90,7 +121,10 @@ const RegisterForm = ({ errors, isValid, setFieldValue }) => {
 
 export const EnhancedRegisterForm = withFormik({
   enableReinitialize: true,
-  handleSubmit: () => {},
+  handleSubmit: ({ email, password, confirmPassword }) => {
+    // eslint-disable-next-line no-console
+    console.log({ email, password, confirmPassword });
+  },
   mapPropsToValues: () => ({
     email: "",
     password: "",
