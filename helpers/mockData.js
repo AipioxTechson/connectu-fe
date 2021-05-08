9ac66024d40a9f4946a312b6dad49416a49de94d
Jarrod Servilla <jarrod.servilla@gmail.com>
2021-05-08 19:09:51 -0400

    mock data

---
+++ helpers/mockData.js
@@ -0,0 +1,20 @@
+import { createMockGroupChat, createMockUser } from "./index";
+
+const mockGroupChats = [
+  createMockGroupChat({
+    name: "CSC301",
+    description: "Introduction to Software Engineering",
+    isCommunity: false,
+    links: ["http://discord.gg/420", "http://whatsapp.com/1"],
+    status: "pending",
+    courseInfo: {
+      campus: "UTM",
+      department: "ANT",
+      courseCode: "301",
+      term: "fall",
+      year: 2021,
+    },
+  }),
+];
+
+export default mockGroupChats;
--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,8 +1,10 @@
+// import { gql } from "@apollo/client";
 import React from "react";
 
 import SectionContainer from "../../components/SectionContainer";
+// import client from "../apollo-client";
 
-export default function Admin() {
+export default function Admin({ pending, rejected, banned }) {
   return (
     <div className="page-container">
       <SectionContainer>
@@ -10,4 +12,23 @@ export default function Admin() {
       </SectionContainer>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  /*
+  const { data } = await client.query({
+    query: gql`
+      query hello {
+        hello
+      }
+    `,
+  }); */
+  const data = { pending: [], rejected: [], banned: [] };
+  return {
+    props: {
+      pending: data.pending,
+      rejected: data.rejected,
+      banned: data.banned,
+    },
+  };
 }
