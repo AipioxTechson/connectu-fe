74eaaa5a12e67fd2bc53c20e91331ff7e220c9a4
Jarrod Servilla <45128231+jcserv@users.noreply.github.com>
2021-05-08 20:21:51 -0400

    Feature/course info (#6)
    * mock data
    
    * complete course info
    
    * add error msgs

---
+++ components/RequestsList.jsx
@@ -0,0 +1,19 @@
+import { Box, Heading } from "@chakra-ui/react";
+import React from "react";
+
+const AdminChatCard = ({ name }) => <Box>{name}</Box>;
+
+const AdminList = ({ heading, items }) => (
+  <div className="col-6">
+    <div className="row-12">
+      <Heading as="h2">{heading}</Heading>
+    </div>
+    <div className="row-12">
+      {items.map((chat, index) => (
+        <AdminChatCard key={index} {...chat} />
+      ))}
+    </div>
+  </div>
+);
+
+export default AdminList;
--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,13 +1,35 @@
+// import { gql } from "@apollo/client";
 import React from "react";
 
 import SectionContainer from "../../components/SectionContainer";
+// import client from "../apollo-client";
 
-export default function Admin() {
+export default function Admin({ pending, rejected, banned }) {
+  const yeet = pending || rejected || banned;
   return (
     <div className="page-container">
       <SectionContainer>
-        <div>Admin</div>
+        <div>Admin {yeet}</div>
       </SectionContainer>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  /*
+  const { data } = await client.query({
+    query: gql`
+      query hello {
+        hello
+      }
+    `,
+  }); */
+  const data = { pending: [], rejected: [], banned: [] };
+  return {
+    props: {
+      pending: data.pending,
+      rejected: data.rejected,
+      banned: data.banned,
+    },
+  };
 }
