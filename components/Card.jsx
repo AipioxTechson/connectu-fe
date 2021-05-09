e755d5174fdf8dad4422a19ecc60ddc6e1ac684d
Jarrod Servilla <45128231+jcserv@users.noreply.github.com>
2021-05-09 10:49:27 -0400

    V0.9 (#20)
    * modify footer, add pages
    
    * update paths
    
    * chore - remove package-lock
    
    * Feature/signup (#2)
    
    * add bootstrap
    
    * base for register form
    
    * register form donezo
    
    * comment out community
    
    * rename community to chat + comment out gql
    
    * add validation
    
    * Feature/modal (#3)
    
    * basic modal
    
    * add/remove extra link fields
    
    * add section container
    
    * add validation
    
    * update fr locale and add (janky) validation to links
    
    * V0.1 (#4) (#5)
    
    * modify footer, add pages
    
    * update paths
    
    * chore - remove package-lock
    
    * Feature/signup (#2)
    
    * add bootstrap
    
    * base for register form
    
    * register form donezo
    
    * comment out community
    
    * rename community to chat + comment out gql
    
    * add validation
    
    * Feature/modal (#3)
    
    * basic modal
    
    * add/remove extra link fields
    
    * add section container
    
    * add validation
    
    * update fr locale and add (janky) validation to links
    
    * Feature/course info (#6)
    
    * mock data
    
    * complete course info
    
    * add error msgs
    
    * Group chat information page (#8)
    
    * Group chat information page (skeleton?)
    
    * add swag
    
    Co-authored-by: Jarrod Servilla <jarrod.servilla@gmail.com>
    
    * frontend for admin done (#9)
    
    * I created the hero page. (#11)
    
    * I created the hero page
    
    * add register functionality (#12)
    
    * Feature/login (#13)
    
    * login form FE done
    
    * hook up to backend
    
    * add cookies
    
    * merge develop
    
    * attempt to fix the integration (#15)
    
    * attempt to fix the integration
    
    * change to https
    
    * Feature/integration (#16)
    
    * change nav display based on auth
    
    * hook up create group chat modal
    
    * add search (#18)
    
    * add search
    
    * add margins
    
    * Feature/integration (#19)
    
    * change nav display based on auth
    
    * hook up create group chat modal
    
    * hook up chat page
    
    * add dynamic title + desc
    
    * add toasts
    
    * add hate speech filter
    
    * change schema
    
    * change default values
    
    * remove linting
    
    Co-authored-by: Nina Ricci Lu <nrlu29@gmail.com>
    Co-authored-by: Nina Ricci Lu <itsninaricci.lu@mail.utoronto.ca>
    Co-authored-by: Ritvik Bhardwaj <43301959+AipioxTechson@users.noreply.github.com>

---
+++ components/Card.jsx
@@ -0,0 +1,45 @@
+import { Box, Image } from "@chakra-ui/react";
+import React from "react";
+import { FaDiscord, FaWhatsapp } from "react-icons/fa";
+import Tilt from "react-vanilla-tilt";
+
+export const Card = ({ name, description, links }) => {
+  const LinksMapped = links.map((link) => {
+    if (link.includes("discord")) {
+      return { type: "discord" };
+    }
+    return { type: "whatsapp" };
+  });
+  return (
+    <Tilt>
+      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+        <Box justifyContent="center">
+          <Image
+            src="https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=400&fit=max"
+            alt="CourseImage"
+          />
+        </Box>
+
+        <Box p="6">
+          <Box fontWeight="semibold" as="h3" lineHeight="tight">
+            {name}
+          </Box>
+
+          <Box mt="1" fontWeight="semibold" as="h4" isTruncated>
+            {description}
+          </Box>
+          <Box mt="2">
+            {LinksMapped.map(({ type }, index) =>
+              type === "discord" ? (
+                <FaDiscord key={index} />
+              ) : (
+                <FaWhatsapp key={index} />
+              )
+            )}
+          </Box>
+        </Box>
+      </Box>
+    </Tilt>
+  );
+};
+export default Card;
--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -125,7 +125,7 @@ const RegisterForm = ({
 
 export const EnhancedRegisterForm = withFormik({
   enableReinitialize: true,
-  handleSubmit: async ({ email, password }) => {
+  handleSubmit: async ({ email, password }, { props: { toast } }) => {
     const {
       data: {
         signup: { status, jwtToken },
@@ -143,8 +143,14 @@ export const EnhancedRegisterForm = withFormik({
     });
 
     if (status === "USER_EXISTS") {
-      // eslint-disable-next-line no-alert
-      alert("USER ALREADY EXISTS");
+      toast({
+        title: "An error has occurred",
+        description: "User already exists",
+        position: "bottom-left",
+        status: "error",
+        duration: 5000,
+        isCloseable: false,
+      });
     } else {
       cookie.set("email", email, 24);
       cookie.set("jwtToken", jwtToken, 24);
