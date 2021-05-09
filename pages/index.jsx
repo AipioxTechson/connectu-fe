c78e2f3fadc13bebb4877bc401698266e3c8658d
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-05-09 04:20:39 -0400

    add search

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,97 @@
-// import { gql } from "@apollo/client";
-import { Button, Heading, Img, Text } from "@chakra-ui/react";
-import React from "react";
+import { gql } from "@apollo/client";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Img,
+  Input,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
-// import client from "../apollo-client";
+import client from "../apollo-client";
+import { Card } from "../components/Card";
 
-export default function Home() {
+export default function Home({
+  groupChats: { groupChats, totalPages, pageNumber },
+}) {
+  const [currentPage, setCurrentPage] = useState(pageNumber);
+  const [totalPageState, setTotalPage] = useState(totalPages);
+  const [groupChatStates, setGroupChats] = useState(groupChats);
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [oldSearchQuery, setOldSearchQuery] = useState("");
+
+  const handleSearch = async () => {
+    setCurrentPage(0);
+    setOldSearchQuery(searchQuery);
+    const {
+      data: {
+        groupChats: {
+          groupChats: newGroupChats,
+          totalPages: newTotalPages,
+          pageNumber: newPageNumber,
+        },
+      },
+    } = await client.query({
+      query: gql`
+        query searchGroupChats($text: String, $isCommunity: Boolean) {
+          groupChats: searchGroupChats(text: $text, isCommunity: $isCommunity) {
+            groupChats {
+              name
+              description
+              links
+            }
+            totalPages
+            pageNumber
+          }
+        }
+      `,
+      variables: {
+        text: searchQuery,
+        isCommunity: false,
+      },
+    });
+    setGroupChats([...newGroupChats]);
+    setTotalPage(newTotalPages);
+    setCurrentPage(newPageNumber);
+  };
+
+  const displayMorePages = async () => {
+    setCurrentPage((page) => page + 1);
+    const {
+      data: {
+        groupChats: {
+          groupChats: newGroupChats,
+          totalPages: newTotalPages,
+          pageNumber: newPageNumber,
+        },
+      },
+    } = await client.query({
+      query: gql`
+        query searchGroupChats($page: Float, $text: String) {
+          groupChats: searchGroupChats(page: $page, text: $text) {
+            groupChats {
+              name
+              description
+              links
+            }
+            totalPages
+            pageNumber
+          }
+        }
+      `,
+      variables: {
+        page: currentPage + 1,
+        text: oldSearchQuery,
+      },
+    });
+    setGroupChats((oldGroupChats) => [...oldGroupChats, ...newGroupChats]);
+    setTotalPage(newTotalPages);
+    setCurrentPage(newPageNumber);
+  };
   return (
     <div className="page-container">
       <div className="d-flex row-12 justify-content-center">
@@ -26,23 +113,63 @@ export default function Home() {
           <Img alt="Chat image" src="/smartphone.png" />
         </div>
       </div>
+      <Spacer />
+      <Spacer />
+      <div className="col-12 align-items-center justify-self-center">
+        <Text fontSize="md" color="grey" m={3}>
+          FIND GROUPCHATS
+        </Text>
+        <Heading as="h2" size="2xl" m={3}>
+          Discover
+        </Heading>
+      </div>
+      <div className="col-11">
+        <Input
+          placeholder="Search"
+          value={searchQuery}
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+          }}
+        />
+        <Box textAlign="center">
+          <Button onClick={handleSearch}>Search</Button>
+        </Box>
+        <Flex wrap="wrap" justifyContent="flex-start">
+          {groupChatStates.map((groupChat, index) => (
+            <Card key={index} {...groupChat} />
+          ))}
+        </Flex>
+        {currentPage !== totalPageState ? (
+          <Box textAlign="center">
+            <Button onClick={displayMorePages}>View More</Button>
+          </Box>
+        ) : null}
+      </div>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  /*
-  const { data } = await client.query({
+  const {
+    data: { groupChats },
+  } = await client.query({
     query: gql`
-      query hello {
-        hello
+      query getGroupChats {
+        groupChats: getGroupChats {
+          groupChats {
+            name
+            description
+            links
+          }
+          totalPages
+          pageNumber
+        }
       }
     `,
-  }); */
-  const data = { hello: "hello" };
+  });
   return {
     props: {
-      data,
+      groupChats,
     },
   };
 }
